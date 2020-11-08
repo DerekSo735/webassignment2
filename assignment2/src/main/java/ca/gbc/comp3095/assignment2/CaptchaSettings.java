6c042230ef5001e86c78431cfcbbe426359401ee
DerekSo735 <71353573+DerekSo735@users.noreply.github.com>
2020-11-08 01:20:25 -0500

    Add files via upload

---
+++ assignment2/src/main/java/ca/gbc/comp3095/assignment2/CaptchaSettings.java
@@ -0,0 +1,34 @@
+package ca.gbc.comp3095.assignment2;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "google.recaptcha.key")
+public class CaptchaSettings {
+
+    private String site;
+    private String secret;
+
+    public void setSite(String site) {
+        this.site = site;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+
+
+
+    public String getSite() {
+        return site;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+
+
+
+}
