2541b525a97da805d7d1500408a206a46da1c052
Luckyunluck <magomed.alimuradovv@gmail.com>
2020-11-08 23:12:03 -0500

    added db

---
+++ assignment2/target/classes/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO TBL_USERS (first_name, last_name, address, email, password) VALUES
+    ('Admin', 'Admin', 'some address, NA', 'admin', 'admin'),
+    ('Deja', 'Vu', 'other address, ON', 'xyz@email.com', 'qwerty123'),
+    ('Caption', 'America', 'another address','cap@marvel.com', 'verystrongpassword');
